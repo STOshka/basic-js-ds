7f98fd83d23ab2c73e936bdaa26b4e09f8ee84b0
STOshka <34401821+STOshka@users.noreply.github.com>
2022-04-28 00:30:43 +0300

    feat: add BinarySearchTree (not fully clear)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,47 +1,101 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
-*/
+*/  
 class BinarySearchTree {
 
+  constructor() {
+      this.firstElement = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.firstElement;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.firstElement == null) {
+      this.firstElement = new Node(data);
+    }
+    else {
+      let element = this.firstElement;
+        while (true) {
+          if (element.data > data) {
+            if (element.left != null) {
+              element = element.left;
+            } else { 
+              element.left = new Node(data);
+              break;
+            }
+          } else {
+            if (element.right != null) {
+              element = element.right;
+            } else { 
+              element.right = new Node(data);
+              break;
+          }
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let element = this.firstElement;
+    while (true) {
+      console.log(element.data);
+      if (element.data == data) return true;
+      element = element.data > data ? element.left : element.right; 
+      if (element == null) return false;
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let element = this.firstElement;
+    while (true) {
+      if (element.data == data) return { data: element.data };
+      element = element.data > data ? element.left : element.right; 
+      if (element == null) return null;
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let element = this.firstElement;
+    let prevElement = null;
+    while (true) {
+      if (element.data == data) {
+        if (element == this.firstElement) {
+          let right = element.right;
+          this.firstElement = element.left;
+          this.firstElement.right = right;
+          break;
+        } else {
+          console.log('prevElement', prevElement);
+          if (prevElement.left != null && prevElement.left.data == data) prevElement.left = element.left;
+          else prevElement.right = element.right;
+          break;
+        }
+      }
+      prevElement = element;
+      element = element.data > data ? element.left : element.right; 
+      if (element == null) break;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let element = this.firstElement;
+    while (element.left != null) element = element.left;
+    return element.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let element = this.firstElement;
+    while (element.right != null) element = element.right;
+    return element.data;
   }
+
 }
 
 module.exports = {
